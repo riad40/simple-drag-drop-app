5cf433880081f9b493bc015c2cff86910be85bfd
riad40 <riadabdelaziz40@gmail.com>
2023-05-04 12:18:24 +0100

    create ProjectInput class - make the logic of rednering the form

--- public/index.js
+++ public/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("init project");
+// ProjectInput Class
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.querySelector('#project-input');
+        this.hostElement = document.querySelector('#app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.formElement = importedNode.firstElementChild;
+        this.formElement.id = 'user-input';
+        this.titleInputElement = this.formElement.querySelector('#title');
+        this.descriptionInputElement = this.formElement.querySelector('#description');
+        this.peopleInputElement = this.formElement.querySelector('#people');
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
+    }
+}
+// instantiate classes
+const prjInput = new ProjectInput();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("init project")
+// ProjectInput Class
+class ProjectInput {
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    formElement: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
+
+    constructor() {
+        this.templateElement = document.querySelector('#project-input')!
+        this.hostElement = document.querySelector('#app')!
+
+        const importedNode = document.importNode(this.templateElement.content, true)
+
+        this.formElement = importedNode.firstElementChild as HTMLFormElement
+
+        this.formElement.id = 'user-input'
+
+        this.titleInputElement = this.formElement.querySelector('#title') as HTMLInputElement
+        this.descriptionInputElement = this.formElement.querySelector('#description') as HTMLInputElement
+        this.peopleInputElement = this.formElement.querySelector('#people') as HTMLInputElement
+
+        this.attach()
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.formElement)
+    }
+}
+
+// instantiate classes
+const prjInput = new ProjectInput()
